316fb42f4f09ae26e7e87c61f71b0a6b0948b1ab
Md. Mehedi Hasan <mehedihasan0937580@gmail.com>
2025-02-28 16:45:22 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# **sales-dashboard design** using -> 
+> React, tailwindCSS, MUI, Framer-motion and Recharts
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+function App() {
+  return (
+    <>
+      <div className="flex flex-col gap-4 justify-center items-center h-screen font-semibold text-4xl ">
+        <h1>welcome to dashboard</h1>
+        <Button variant="outlined">Try again</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
